246b50e0b8f1147b1970c2980e43843d4b313c12
jcastro27 <joeysk8ter@hotmail.com>
2022-07-02 22:18:41 -0500

    Initial Commit

---
+++ CoreData1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData1.1
+//
+//  Created by Joseph Castro on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
